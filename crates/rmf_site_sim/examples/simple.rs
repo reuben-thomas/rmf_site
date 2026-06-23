8cbb517b003a4d8cb7bbf8b0515b112b78725c52
Reuben Thomas <reubenthomas123@gmail.com>
2026-06-23 10:55:56 +0800

    Add component sync

--- crates/rmf_site_sim/examples/simple.rs
+++ crates/rmf_site_sim/examples/simple.rs
@@ -1,8 +1,13 @@
 use bevy::prelude::*;
 use rmf_site_sim::{
-    EndCondition, Simulation, SimulationPlugin, SimulationSet, time::SimulationTime,
+    EndCondition, SimulationBuilder, SimulationPlugin, sync::EntityCloner, time::SimulationTime,
 };
 use std::time::Duration;
+
+#[derive(Component, Clone, Debug)]
+struct TurtleBot {
+    name: String,
+}
 
 fn main() {
     App::new()
@@ -15,13 +20,39 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    let set = SimulationSet;
-    let set_entity = commands.spawn(set).id();
-    commands.spawn::<Simulation>(set.run(
+fn sim_startup(mut commands: Commands) {
+    info!("startup - Spawning robots...");
+    commands.spawn(TurtleBot {
+        name: "TurtleBot1".to_string(),
+    });
+    commands.spawn(TurtleBot {
+        name: "TurtleBot2".to_string(),
+    });
+}
+
+fn sim_update(query: Query<(Entity, &TurtleBot)>) {
+    info!(
+        "compute - Found {} robots in the simulation.",
+        query.iter().count()
+    );
+}
+
+fn setup(world: &mut World) {
+    let mut entity_cloner = EntityCloner::default();
+    entity_cloner.register::<TurtleBot>();
+
+    let set = SimulationBuilder::new()
+        .add_startup_systems(sim_startup)
+        .add_compute_systems(sim_update);
+    let set_entity = world.spawn_empty().id();
+
+    // Robots are spawned by the sim_startup system inside the sim world.
+    let simulation = set.build(
         "Primary 0..10".to_string(),
         set_entity,
         EndCondition::Time(SimulationTime::new(Duration::from_secs(10))),
-        Schedule::new(Update),
-    ));
+        &mut entity_cloner,
+        world,
+    );
+    world.spawn(simulation);
 }
