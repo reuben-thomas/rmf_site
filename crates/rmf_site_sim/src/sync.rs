99e92d6ab02e6a75a3d8803474c24ff0441b7516
Reuben Thomas <reubenthomas123@gmail.com>
2026-06-29 12:29:08 +0800

    Move collect, insert to private static methods
    Signed-off-by: Reuben Thomas <reubenthomas123@gmail.com>

--- crates/rmf_site_sim/src/sync.rs
+++ crates/rmf_site_sim/src/sync.rs
@@ -12,18 +12,31 @@ impl EntityCloner {
     pub fn register<T: Component + Clone>(&mut self) {
         self.cloners.push(ComponentCloner {
             clone_to_sim: |entity_map, main_world, sim_world| {
-                for (main_entity, component) in collect_components::<T>(main_world) {
+                for (main_entity, component) in Self::collect_components::<T>(main_world) {
                     let sim_entity = entity_map.get_or_spawn_sim(main_entity, sim_world);
-                    insert_component(sim_world, sim_entity, component);
+                    Self::insert_component(sim_world, sim_entity, component);
                 }
             },
             clone_to_main: |entity_map, sim_world, main_world| {
-                for (sim_entity, component) in collect_components::<T>(sim_world) {
+                for (sim_entity, component) in Self::collect_components::<T>(sim_world) {
                     let main_entity = entity_map.get_or_spawn_main(sim_entity, main_world);
-                    insert_component(main_world, main_entity, component);
+                    Self::insert_component(main_world, main_entity, component);
                 }
             },
         });
+    }
+
+    fn collect_components<T: Component + Clone>(world: &World) -> Vec<(Entity, T)> {
+        world
+            .iter_entities()
+            .filter_map(|entity| Some((entity.id(), entity.get::<T>().cloned()?)))
+            .collect()
+    }
+
+    fn insert_component<T: Component>(world: &mut World, entity: Entity, component: T) {
+        if let Ok(mut e) = world.get_entity_mut(entity) {
+            e.insert(component);
+        }
     }
 
     pub fn clone_to_sim(&mut self, main_world: &World, sim_world: &mut World) {
@@ -73,17 +86,4 @@ impl EntityMap {
 struct ComponentCloner {
     clone_to_sim: fn(&mut EntityMap, &World, &mut World),
     clone_to_main: fn(&mut EntityMap, &World, &mut World),
-}
-
-fn collect_components<T: Component + Clone>(world: &World) -> Vec<(Entity, T)> {
-    world
-        .iter_entities()
-        .filter_map(|entity| Some((entity.id(), entity.get::<T>().cloned()?)))
-        .collect()
-}
-
-fn insert_component<T: Component>(world: &mut World, entity: Entity, component: T) {
-    if let Ok(mut e) = world.get_entity_mut(entity) {
-        e.insert(component);
-    }
 }
