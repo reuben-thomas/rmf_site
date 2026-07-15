93ae43b107953a68a085f79f6204c2e19b882cff
Reuben Thomas <reubenthomas123@gmail.com>
2026-07-16 06:42:29 +0800

    Fix playback speed
    Signed-off-by: Reuben Thomas <reubenthomas123@gmail.com>

--- crates/rmf_site_sim/src/playback.rs
+++ crates/rmf_site_sim/src/playback.rs
@@ -1,14 +1,16 @@
 use crate::simulation::Simulation;
 use bevy::prelude::*;
+use std::time::Duration;
 
-const STEP_DURATION_SECONDS: f32 = 0.25;
-const PAUSE_BEFORE_REPLAY_SECONDS: f32 = 2.0;
+const PLAYBACK_SPEED: f32 = 5.0;
+const PAUSE_BEFORE_REPLAY_SECONDS: f32 = 1.0;
 
 pub struct SimulationPlaybackPlugin;
 
 impl Plugin for SimulationPlaybackPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<PlaybackState>()
+            .init_resource::<Play>()
             .add_systems(Update, play.run_if(in_state(PlaybackState::Playing)))
             .add_systems(Update, pause.run_if(in_state(PlaybackState::Paused)))
             .add_systems(OnEnter(PlaybackState::Paused), start_pause)
@@ -23,55 +25,50 @@ enum PlaybackState {
     Paused,
 }
 
+#[derive(Resource, Default)]
 struct Play {
-    timer: Timer,
+    elapsed: Duration,
     step_idx: usize,
 }
 
-impl Default for Play {
-    fn default() -> Self {
-        Self {
-            timer: Timer::from_seconds(STEP_DURATION_SECONDS, TimerMode::Repeating),
-            step_idx: 0,
+fn play(world: &mut World) {
+    world.resource_scope(|world, mut play: Mut<Play>| {
+        play.elapsed += world.resource::<Time>().delta().mul_f32(PLAYBACK_SPEED);
+
+        // TODO: Handle multiple simulations
+        let mut query = world.query::<&mut Simulation>();
+        let mut simulation = query.single_mut(world).unwrap();
+        let steps = simulation.steps();
+
+        if play.step_idx >= steps.len() {
+            play.step_idx = 0;
+            play.elapsed = Duration::ZERO;
+            world
+                .resource_mut::<NextState<PlaybackState>>()
+                .set(PlaybackState::Paused);
+            return;
         }
-    }
-}
 
-#[derive(Resource)]
-struct PauseTimer(Timer);
-
-fn play(world: &mut World, mut state: Local<Play>) {
-    state.timer.tick(world.resource::<Time>().delta());
-    if !state.timer.just_finished() {
-        return;
-    }
-
-    // TODO: Handle multiple simulations
-    let mut query = world.query::<&mut Simulation>();
-    let mut simulation = query.single_mut(world).unwrap();
-    let events = simulation
-        .steps()
-        .values()
-        .nth(state.step_idx)
-        .map(|step| step.events.clone());
-    match events {
-        Some(events) => {
-            state.step_idx += 1;
+        let step_events: Vec<_> = steps
+            .iter()
+            .skip(play.step_idx)
+            .take_while(|(time, _)| time.elapsed() <= play.elapsed)
+            .map(|(_, step)| step.events.clone())
+            .collect();
+        play.step_idx += step_events.len();
+        for events in step_events {
             for event in events {
                 event.apply(world);
             }
         }
-        None => {
-            state.step_idx = 0;
-            world
-                .resource_mut::<NextState<PlaybackState>>()
-                .set(PlaybackState::Paused);
-        }
-    }
+    });
 }
 
+#[derive(Resource)]
+struct Pause(Timer);
+
 fn start_pause(mut commands: Commands) {
-    commands.insert_resource(PauseTimer(Timer::from_seconds(
+    commands.insert_resource(Pause(Timer::from_seconds(
         PAUSE_BEFORE_REPLAY_SECONDS,
         TimerMode::Once,
     )));
@@ -79,7 +76,7 @@ fn start_pause(mut commands: Commands) {
 
 fn pause(
     time: Res<Time>,
-    mut timer: ResMut<PauseTimer>,
+    mut timer: ResMut<Pause>,
     mut next_state: ResMut<NextState<PlaybackState>>,
 ) {
     timer.0.tick(time.delta());
@@ -89,7 +86,7 @@ fn pause(
 }
 
 fn reset(world: &mut World) {
-    world.remove_resource::<PauseTimer>();
+    world.remove_resource::<Pause>();
     let mut query = world.query::<&mut Simulation>();
     let mut simulation = query.single_mut(world).unwrap();
     let events = simulation.init_step().events.clone();
