50fb47e8a368f8f7fab0d830739b1fbeb9d91091
Reuben Thomas <reubenthomas123@gmail.com>
2026-07-16 11:55:16 +0800

    Populate module level docs
    Signed-off-by: Reuben Thomas <reubenthomas123@gmail.com>

--- crates/rmf_site_sim/src/lib.rs
+++ crates/rmf_site_sim/src/lib.rs
@@ -15,17 +15,27 @@
  *
 */
 
-//! Discrete event simulation for the [rmf_site_editor](https://github.com/open-rmf/rmf_site).
+//! # rmf_site_sim
 //!
-//! TODO
-//! | Concept | Representation | Description |
+//! `rmf_site_sim` provides the ability to perform
+//! [discrete-event simulation](https://en.wikipedia.org/wiki/Discrete-event_simulation) (DES) in Bevy,
+//! and is primarily intended for use with the [rmf_site_editor](https://github.com/open-rmf/rmf_site).
+//!
+//! The following illustrates how the core components of a DES are represented, or can be accessd in
+//! this library.
+//!
+//! | Concept | Definition | Representation |
 //! | --- | --- | --- |
-//! | System state | Bevy `World` | -
-//! | Model | Bevy system | -
-//! | Event | [`event::DiscreteEvent`] / [`bevy::prelude::Command`] | -
-//! | Event scheduling | [`event::DiscreteChangeWriter`], [`event::DiscreteComponentWriter`], [`event::DiscreteResourceWriter`] | -
-//! | Future event list | [`event::DiscreteEvents`] | -
-//! | Simulation output | [`SimulationStep`] | - 
+//! | System | A collection of entities being modelled over time. | - |
+//! | System Model | An abstract representation of the system. | A collection of Bevy systems ([`bevy::prelude::System`]) configured in a [`SimulationBuilder``]. |
+//! | System State | A state vector describing the state of the world at any time-instance. | Entities, components, and resources in [`bevy::prelude::World`]. |
+//! | Entity | An instance that requires representation in the system model. | [`bevy::prelude::Entity`] |
+//! | Attributes | Properties of an entity, or the system as a whole. | [`bevy::prelude::Component`], [`bevy::prelude::Resource`] |
+//! | Event | An instantaneous occurrence that changes the state of the system. | An [`event::DiscreteEvent`], implemented automatically for any cloneable [`bevy::prelude::Command`]. |
+//! | Event Notice | A record of an event that may occur at some simulation time and the necessary parameters to execute it. | Created using the [`event::DiscreteChangeWriter`], [`event::DiscreteComponentWriter`], and [`event::DiscreteResourceWriter`]. |
+//! | Future Event List (FEL) | A list of event notices for future events, ordered by time of occurrence. | The [`event::DiscreteEvents`] resource holds a list of event notices.. |
+//! | Clock | A variable representing the current value of simulated time. | The [`compute::SimulationComputeClock`] resource. |
+//! | Output | Data produced by running the simulation. | A [`SimulationStep`] for each computed simulation time, collected by the [`Simulation`] component. |
 pub mod compute;
 pub mod event;
 pub mod playback;
