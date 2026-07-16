3fd583c62a991d8b7b0756527aadf506cc05401c
Reuben Thomas <reubenthomas123@gmail.com>
2026-07-16 10:32:03 +0800

    Add topological sort in schedule build
    Signed-off-by: Reuben Thomas <reubenthomas123@gmail.com>

--- crates/rmf_site_sim/src/schedule.rs
+++ crates/rmf_site_sim/src/schedule.rs
@@ -1,10 +1,13 @@
 use bevy::ecs::schedule::{
     InternedScheduleLabel, IntoScheduleConfigs, LogLevel, NodeId, Schedule, ScheduleBuildError,
     ScheduleBuildPass, ScheduleBuildSettings, ScheduleConfigs, ScheduleGraph, ScheduleLabel,
-    SystemSet, graph::DiGraph,
+    SystemSet,
+    graph::{DiGraph, Direction},
 };
 use bevy::ecs::system::ScheduleSystem;
 use bevy::ecs::world::World;
+use std::any::TypeId;
+use std::collections::{BTreeSet, HashMap, HashSet};
 
 /// The schedule that runs once when the a simulation is started.
 #[derive(Clone, Debug, PartialEq, Eq, Hash, ScheduleLabel)]
@@ -69,17 +72,20 @@ impl ScheduleBuilder {
         let mut schedule = Schedule::new(self.label);
         schedule.set_build_settings(ScheduleBuildSettings {
             ambiguity_detection: LogLevel::Error,
-            hierarchy_detection: LogLevel::Warn,
-            auto_insert_apply_deferred: true,
-            use_shortnames: true,
-            report_sets: true,
+            ..Default::default()
         });
         if let Some(configs) = self.configs {
             schedule.add_systems(configs);
         }
         match self.ordering {
             SystemExecutionOrdering::Total => {
-                schedule.add_build_pass(TotalOrderingPass);
+                schedule.add_build_pass(TotalOrderingBuildPass {
+                    added_system_type_ids: schedule
+                        .graph()
+                        .systems()
+                        .map(|(_, system, _)| system.type_id())
+                        .collect(),
+                });
             }
             SystemExecutionOrdering::Partial => {}
         }
@@ -88,10 +94,12 @@ impl ScheduleBuilder {
 }
 
 /// A schedule build pass that enforces a deterministic total ordering of systems.
-#[derive(Debug)]
-struct TotalOrderingPass;
+#[derive(Debug, Default)]
+struct TotalOrderingBuildPass {
+    added_system_type_ids: HashSet<TypeId>,
+}
 
-impl ScheduleBuildPass for TotalOrderingPass {
+impl ScheduleBuildPass for TotalOrderingBuildPass {
     type EdgeOptions = ();
 
     fn add_dependency(&mut self, _from: NodeId, _to: NodeId, _options: Option<&Self::EdgeOptions>) {
@@ -109,10 +117,108 @@ impl ScheduleBuildPass for TotalOrderingPass {
     fn build(
         &mut self,
         _world: &mut World,
-        _graph: &mut ScheduleGraph,
-        _dependency_flattened: &mut DiGraph,
+        graph: &mut ScheduleGraph,
+        dependency_flattened: &mut DiGraph,
     ) -> Result<(), ScheduleBuildError> {
-        // TODO: Enforce a total ordering of the systems in the schedule
+        let mut node_to_in_degree: HashMap<NodeId, usize> =
+            dependency_flattened.nodes().map(|node| (node, 0)).collect();
+        for (_, to) in dependency_flattened.all_edges() {
+            *node_to_in_degree.get_mut(&to).unwrap() += 1;
+        }
+
+        let mut topological_order: Vec<NodeId> = Vec::with_capacity(node_to_in_degree.len());
+        let mut node_queue: BTreeSet<(String, NodeId)> = node_to_in_degree
+            .iter()
+            .filter(|(_, degree)| **degree == 0)
+            .map(|(node, _)| (TotalOrderingBuildPass::get_system_name(graph, node), *node))
+            .collect();
+
+        while let Some((name, node)) = node_queue.pop_first() {
+            if self.process_system(graph, &node) {
+                println!("Processing {name}...");
+                topological_order.push(node);
+            }
+
+            for outgoing_neighbor in
+                dependency_flattened.neighbors_directed(node, Direction::Outgoing)
+            {
+                let degree = node_to_in_degree.get_mut(&outgoing_neighbor).unwrap();
+                *degree -= 1;
+                if *degree == 0 {
+                    node_queue.insert((
+                        TotalOrderingBuildPass::get_system_name(graph, &outgoing_neighbor),
+                        outgoing_neighbor,
+                    ));
+                }
+            }
+        }
+
+        // TODO: This will add redundant edges.
+        for pair in topological_order.windows(2) {
+            dependency_flattened.add_edge(pair[0], pair[1]);
+        }
+
         Ok(())
+    }
+}
+
+impl TotalOrderingBuildPass {
+    fn process_system(&self, graph: &ScheduleGraph, id: &NodeId) -> bool {
+        graph
+            .get_system_at(*id)
+            .is_some_and(|system| self.added_system_type_ids.contains(&system.type_id()))
+    }
+
+    fn get_system_name(graph: &ScheduleGraph, id: &NodeId) -> String {
+        graph
+            .get_system_at(*id)
+            .map(|system| system.name().to_string())
+            .unwrap_or_default()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::event::{DiscreteChangeWriter, DiscreteResourceWriter};
+    use bevy::ecs::resource::Resource;
+    use bevy::ecs::system::{IntoSystem, Res, System};
+
+    #[derive(Clone, Debug, PartialEq, Eq, Hash, ScheduleLabel)]
+    struct TestSchedule;
+
+    #[derive(Resource, Default, Clone)]
+    struct X<const N: usize>;
+
+    fn a(_x1: Res<X<1>>, _writer: DiscreteChangeWriter) {}
+
+    fn b(_x1: Res<X<1>>, _writer: DiscreteChangeWriter) {}
+
+    fn c(_x1: Res<X<1>>, _x2: Res<X<2>>, _writer: DiscreteResourceWriter<X<3>>) {}
+
+    fn get_system_name<M>(system: impl IntoSystem<(), (), M>) -> String {
+        IntoSystem::into_system(system).name().to_string()
+    }
+
+    #[test]
+    fn test_total_ordering() {
+        let mut world = World::new();
+        world.init_resource::<X<1>>();
+        world.init_resource::<X<2>>();
+
+        let mut schedule = ScheduleBuilder::new(TestSchedule)
+            .add_systems((a, b.before(a), c.after(b)))
+            .set_ordering(SystemExecutionOrdering::Total)
+            .build();
+        schedule.initialize(&mut world).unwrap();
+
+        let expected = vec![get_system_name(b), get_system_name(a), get_system_name(c)];
+        let actual: Vec<_> = schedule
+            .systems()
+            .unwrap()
+            .map(|(_, system)| system.name().to_string())
+            .filter(|name| expected.contains(name))
+            .collect();
+        assert_eq!(actual, expected);
     }
 }
